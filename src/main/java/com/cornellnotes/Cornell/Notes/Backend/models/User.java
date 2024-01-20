96a2c7d877c495d1f040a69020903c1be89e180b
jaberisamjaber <jaberjaber018@gmail.com>
2024-01-19 19:11:32 -0500

    created all models for tables in hibernate

--- src/main/java/com/cornellnotes/Cornell/Notes/Backend/models/User.java
+++ src/main/java/com/cornellnotes/Cornell/Notes/Backend/models/User.java
@@ -1,70 +1,117 @@
 package com.cornellnotes.Cornell.Notes.Backend.models;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "user_table")
+@Table(name = "users")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private int idUser;
 	private String firstName;
 	private String lastName;
-	
-	
-	public User() {
-		super();
+	private String password;
+	private String email;
+	private String image;
+	private String role;
+	private String token;
+	private int googleId;
+	@OneToMany(mappedBy = "idNote", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<Note> notes;
+
+	//	Getters and Setters
+	public int getIdUser() {
+		return idUser;
 	}
 
-
-	public User(int id, String firstName, String lastName) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
 	}
-
-
-	public int getId() {
-		return id;
-	}
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	public String getPassword() {
+		return password;
+	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public int getGoogleId() {
+		return googleId;
+	}
+
+	public void setGoogleId(int googleId) {
+		this.googleId = googleId;
 	}
 	
 	
+	@JsonManagedReference
+	public Set<Note> getNotes() {
+		return notes;
+	}
+
+	public void setNotes(Set<Note> notes) {
+		this.notes = notes;
+	}
 
 }
