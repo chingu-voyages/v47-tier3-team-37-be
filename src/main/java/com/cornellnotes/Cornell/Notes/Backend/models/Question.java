9eb4b99da60cde6eedd5e6c59c7f9c93cb85fa5b
Sean Welsh <s.welsh1314@gmail.com>
2024-02-06 14:39:54 -0500

    updated questions to be text instead of varchar(255)

--- src/main/java/com/cornellnotes/Cornell/Notes/Backend/models/Question.java
+++ src/main/java/com/cornellnotes/Cornell/Notes/Backend/models/Question.java
@@ -2,13 +2,7 @@ package com.cornellnotes.Cornell.Notes.Backend.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Questions")
@@ -22,6 +16,7 @@ public class Question {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idQuestion;
+	@Column(columnDefinition = "TEXT")
 	private String content;
 	@ManyToOne
 	@JoinColumn(name = "idNote")
